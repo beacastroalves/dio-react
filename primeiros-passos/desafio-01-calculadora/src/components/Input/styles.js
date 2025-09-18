c4ae7a88506aadbfef7b6913e1e31a0ca67550e9
beacastroalves <beacastro1008@gmail.com>
2025-09-18 15:03:38 +0100

    primeiras estilizações usando styled-components

---
+++ primeiros-passos/desafio-01-calculadora/src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #440055;
+  color: #f9f9f9;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #440055;
+    color: #f9f9f9;
+    border: none;
+    text-align: right;
+    font-size: 24px;
+    padding: 0 8px;
+  }
+`
